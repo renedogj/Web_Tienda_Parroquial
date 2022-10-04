c379a52e7b610eeb0e426af3fba8a76410f1eb2c
renedogj <renedogj@gmail.com>
2022-10-04 15:04:41 +0200

    fix inicioSesion

--- admin/controllers/iniciarSesion.js
+++ admin/controllers/iniciarSesion.js
@@ -19,7 +19,7 @@ function cerrarSesion(){
 }
 
 $(document).ready(() => {
-	$("#bttnAcceder").click(() => {
+	$("#formularioAcceder").submit(() => {
 		var email = $("#correo").val();
 		var password = $("#password").val();
 
--- admin/index.php
+++ admin/index.php
@@ -1,28 +1,28 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Iniciar Sesión</title>
-		<meta name=author content="Javier Renedo">
-		
-		<script src="https://cdnjs.cloudflarecom/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Iniciar Sesión</title>
+	<meta name=author content="Javier Renedo">
+	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 
-		<link rel="shortcut icon" type="png" href="../imagenes/parroquia_200x200.jpg">
-		
-		<link rel="stylesheet" type="text/css" href="../css/body.css">
-		<link rel="stylesheet" type="text/css" href="../css/menuAdministracion.css">
-		<link rel="stylesheet" type="text/css" href="../css/iniciarSesionAdministracion.css"/>
-	</head>
-	<body>
-		<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js"></script>
-		<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-database.js"></script>
-		<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-auth.js"></script>
-		<script type="text/javascript" src="controllers/iniciarSesion.js"></script>
+	<link rel="shortcut icon" type="png" href="../imagenes/parroquia_200x200.jpg">
+	
+	<link rel="stylesheet" type="text/css" href="../css/body.css">
+	<link rel="stylesheet" type="text/css" href="../css/menuAdministracion.css">
+	<link rel="stylesheet" type="text/css" href="../css/iniciarSesionAdministracion.css"/>
+</head>
+<body>
+	<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-app.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-database.js"></script>
+	<script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-auth.js"></script>
+	<script type="text/javascript" src="controllers/iniciarSesion.js"></script>
 
-		<?php
-		include_once "views/barraNavegacion.php";
-		include_once "views/iniciarSesion.html";
-		?>
-	</body>
+	<?php
+	include_once "views/barraNavegacion.php";
+	include_once "views/iniciarSesion.html";
+	?>
+</body>
 </html>
