d4960806c7019c3a2e0d2977af86d9cedd0a1857
renedogj <renedogj@gmail.com>
2022-10-17 23:36:37 +0200

    fix css

--- admin/controllers/listadoCategorias.js
+++ admin/controllers/listadoCategorias.js
@@ -2,11 +2,10 @@ $.ajax({
 	method: "POST",
 	url: "../../models/obtenerCategorias.php",
 	success: function(categorias){
-		//console.log(categorias);
 		let textoMostrarCategoria;
 		for(let i in categorias){
 			if(categorias[i].mostrar_categoria != 0){
-				textoMostrarCategoria = "Si se muestran los articulos de esta categoria";
+				textoMostrarCategoria = "Se muestran los articulos de esta categoria";
 			}else{
 				textoMostrarCategoria = "No se muestran los articulos de esta categoria";
 			}
@@ -17,11 +16,11 @@ $.ajax({
 						$("<button>").text("Editar").click(() => {
 							window.location.assign("editarCategoria.php?id="+categorias[i].Id);
 						})
-						),
+					),
 					$("<td>").text(categorias[i].nombre_categoria),
 					$("<td>").text(textoMostrarCategoria)
-					)
 				)
+			)
 		}
 	},
 	dataType: "json"
