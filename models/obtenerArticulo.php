84763128049e1d74164ca69e73ec0f27c2810de9
renedogj <renedogj@gmail.com>
2023-09-29 00:23:13 +0200

    fix css header

--- models/obtenerArticulo.php
+++ models/obtenerArticulo.php
@@ -6,13 +6,14 @@ include_once "../db/db.php";
 $sql = "SELECT nombre_foto FROM imagenes WHERE ID in (SELECT ID_imagen FROM relacion_producto_imagen WHERE ID_producto = '$idArticulo')";
 $imagenes = simplificarArray($conexion, $sql, "nombre_foto");
 
-$sql = "SELECT nombre_categoria FROM categorias WHERE ID in (SELECT ID_categoria FROM relacion_producto_categoria WHERE ID_producto = '$idArticulo')";
-$categorias = simplificarArray($conexion, $sql, "nombre_categoria");
+$sql = "SELECT id, nombre_categoria FROM categorias WHERE ID in (SELECT ID_categoria FROM relacion_producto_categoria WHERE ID_producto = '$idArticulo')";
+$categorias = obtenerArraySQL($conexion, $sql);
 
 $sql="SELECT id, nombre, descripcion, precio, mostrar, disponibilidad FROM articulos WHERE id='$idArticulo'";
 $articulo = obtenerArraySQL($conexion, $sql)[0];
 
 $articulo["imagenes"] = $imagenes;
 $articulo["categorias"] = $categorias;
+
 echo json_encode($articulo);
 ?>	
